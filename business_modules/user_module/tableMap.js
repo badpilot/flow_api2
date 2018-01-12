4c92da6fdc08bc2912aee4ab7b40e72802c40753
pankai <wspankai@163.com>
2018-01-12 16:46:17 +0800

    daxie

--- business_modules/user_module/tableMap.js
+++ business_modules/user_module/tableMap.js
@@ -1,7 +1,7 @@
 //模块相关数据表
 //user表
-var User = 
-    `create table if not exists User(
+var USER = 
+    `create table if not exists USER(
         user_id INT NOT NULL AUTO_INCREMENT,
         user_name VARCHAR(20) NOT NULL,
         user_real_name VARCHAR(20) NOT NULL,
@@ -12,23 +12,23 @@ var User =
     );
     `
 //角色表
-var Role = 
-    `create table if not exists Role(
+var ROLE = 
+    `create table if not exists ROLE(
         role_id INT NOT NULL AUTO_INCREMENT,
         role_name VARCHAR(20) NOT NULL,
         PRIMARY KEY (role_id)   
     );
     `
-var Role_Access =   
-    `create table if not exist Role_Access(
+var ROLE_ACCESS =   
+    `create table if not exist ROLE_ACCESS(
         role_id INT NOT NULL,
         access_id INT NOT NULL,
         PRIMARY KEY (role_id)
     );
     `
 //权限模块
-var Access_Module =
-    `create table if not exists Access_Module(
+var ACCESS_MODULE =
+    `create table if not exists ACCESS_MODULE(
         module_id INT NOT NULL AUTO_INCREMENT,
         module_name VARCHAR(20) NOT NULL ,
         module_description VARCHAR(50) NOT NULL,
@@ -36,8 +36,8 @@ var Access_Module =
     );
     `
 //角色-权限模块-权限
-var Role_Module_Access = 
-    `create table if not exists Role_Module_Access(
+var ROLE_MODULE_ACCESS = 
+    `create table if not exists ROLE_MODULE_ACCESS(
         access_id INT NOT NULL AUTO_INCREMENT,
         access_name VARCHAR(20) NOT NULL,
         module_id INT NOT NULL,
@@ -47,9 +47,9 @@ var Role_Module_Access =
     `
 
 module.exports = {
-    User: User,
-    Role: Role,
-    Role_Access:Role_Access,
-    Access_Module: Access_Module,
-    Role_Module_Access: Role_Module_Access   
+    USER: USER,
+    ROLE: ROLE,
+    ROLE_ACCESS:ROLE_ACCESS,
+    ACCESS_MODULE: ACCESS_MODULE,
+    ROLE_MODULE_ACCESS: ROLE_MODULE_ACCESS   
 }
